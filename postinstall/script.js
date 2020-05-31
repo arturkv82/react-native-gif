d1e7659d52bfcb8f2ee387c3b6ab4aae5cb42ad5
Artur Kazarov <arturkv82@gmail.com>
2020-05-31 18:21:45 +0300

    added postinstall script.

---
+++ postinstall/script.js
@@ -0,0 +1,20 @@
+// @ts-nocheck
+const os = require('os');
+const platform = os.platform();
+const exec = require('child_process').exec;
+
+function handler(error, stdout, stderr) {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.error(`stderr: ${stderr}`);
+}
+
+if (platform === 'win32') {
+  exec('yarn && yarn run buildwin', handler);
+} else {
+  console.log("exec linux");
+  exec('yarn && yarn build-mac', handler);
+}
